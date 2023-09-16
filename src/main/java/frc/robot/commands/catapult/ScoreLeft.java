5a7aa9fcfcfb48ce753e5fdf4737398b19acdd83
Robert Tuttle <frc2881@gmail.com>
2023-09-16 12:52:35 -0500

    Fix intake and catapult operating at the same time

--- src/main/java/frc/robot/commands/catapult/ScoreLeft.java
+++ src/main/java/frc/robot/commands/catapult/ScoreLeft.java
@@ -10,12 +10,14 @@ import static frc.robot.Constants.Catapult.kShootTimeout;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.LeftCatapult;
 
 public class ScoreLeft extends SequentialCommandGroup {
   public ScoreLeft(LeftCatapult leftCatapult, Intake intake) {
     addCommands(new InstantCommand(() -> intake.shootExtend(), intake),
+                new WaitCommand(1),
                 new ShootLeft(leftCatapult).withTimeout(kShootTimeout),
                 new ResetLeft(leftCatapult).withTimeout(kResetTimeout),
                 new InstantCommand(() -> intake.shootRetract(), intake));
--- src/main/java/frc/robot/commands/catapult/ScoreRight.java
+++ src/main/java/frc/robot/commands/catapult/ScoreRight.java
@@ -10,12 +10,14 @@ import static frc.robot.Constants.Catapult.kShootTimeout;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.RightCatapult;
 
 public class ScoreRight extends SequentialCommandGroup {
   public ScoreRight(RightCatapult rightCatapult, Intake intake) {
     addCommands(new InstantCommand(() -> intake.shootExtend(), intake),
+                new WaitCommand(1),
                 new ShootRight(rightCatapult).withTimeout(kShootTimeout),
                 new ResetRight(rightCatapult).withTimeout(kResetTimeout),
                 new InstantCommand(() -> intake.shootRetract(), intake));
